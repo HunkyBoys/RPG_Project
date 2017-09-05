bf1fa7cd38d9c8e7ec337f102b74eaacf3f9ee4b
Jason <Jason hehexd@DESKTOP-GU773E6>
2017-09-04 20:27:21 -0400

    work

--- RPG_Project/Animations.cs
+++ RPG_Project/Animations.cs
@@ -13,10 +13,25 @@ namespace RPG_Project
 {
     class Animations
     {
-
-        public void Draw(SpriteBatch spriteBatch, Texture2D SpriteSheet, Vector2 position, Vector2 size, int framesX, int framesY)
+        int step = 0;
+        int framesX = 0;
+        public void Update(GameTime gameTime)
         {
-            spriteBatch.Draw(SpriteSheet, position, sourceRectangle: new Rectangle(framesX, framesY, 50, 50));
+            step = gameTime.ElapsedGameTime.Seconds;
+            if (step > 4) {
+                step = 0;
+            }
+            if(step == 0) { framesX = 0; }
+            if (step == 1) { framesX = 50; }
+            if (step == 2) { framesX = 100; }
+            if (step == 3) { framesX = 150; }
+            if (step == 4) { framesX = 200; }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D SpriteSheet, Vector2 position, Vector2 size)
+        {
+            
+            spriteBatch.Draw(SpriteSheet, position, sourceRectangle: new Rectangle(framesX, 0,  50, 50));
         }
     }
 }
